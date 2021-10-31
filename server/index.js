2e3d72579d36b1d16082272b6a8cfdcd178219b0
thimira nirmal <timnirmal@gmail.com>
2021-10-31 07:12:49 +0530

    Before adding Auth

--- server/index.js
+++ server/index.js
@@ -23,6 +23,6 @@ app.get('/', (req, res) => res.send('Hello world!'));
 // use Routes
 app.use('/api/books', books);
 
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
